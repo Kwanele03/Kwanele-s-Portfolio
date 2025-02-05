d11e6213c83760fad581befc223bf3ca13ff8446
Kwanele <112133974+Kwanele03@users.noreply.github.com>
2025-02-05 22:44:23 +0200

    Update SkillsController.java

--- Themyleaf/src/main/java/com/enviro/practice/grad001/kwanelentshele/skills/SkillsController.java
+++ Themyleaf/src/main/java/com/enviro/practice/grad001/kwanelentshele/skills/SkillsController.java
@@ -1,4 +1,4 @@
-package com.enviro.practice.grad001.kwanelentshele.skills;
+apackage com.enviro.practice.grad001.kwanelentshele.skills;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SkillsController {
 	@GetMapping("/skills")
-	public String skill(Model model) {
+	public String skill() {
 		return "skills";
 	}
 
