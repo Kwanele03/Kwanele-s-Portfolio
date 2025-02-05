90a053aaffee522b8f410fb65df7d41130785dd8
Kwanele <112133974+Kwanele03@users.noreply.github.com>
2025-02-05 22:43:14 +0200

    Update ContactsController.java

--- Themyleaf/src/main/java/com/enviro/practice/grad001/kwanelentshele/contacts/ContactsController.java
+++ Themyleaf/src/main/java/com/enviro/practice/grad001/kwanelentshele/contacts/ContactsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ContactsController {
 
 	@GetMapping("/contacts")
-	public String contacts(Model model) {
+	public String contacts() {
 		return "contacts";
 	}
 }
