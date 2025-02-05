c2880ab6e18f60a8a1a5a9d44d6c21ed51961ea1
Kwanele <112133974+Kwanele03@users.noreply.github.com>
2025-02-05 22:43:37 +0200

    Update HomeController.java

--- Themyleaf/src/main/java/com/enviro/practice/grad001/kwanelentshele/controller/HomeController.java
+++ Themyleaf/src/main/java/com/enviro/practice/grad001/kwanelentshele/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	
 	@GetMapping("/index")
-	public String home(Model model) {
+	public String home() {
 		return "index";
 	}
 }
